fd3b5d0d45dd576eadb57012bd6875eb2bef769d
khoadoan91 <kdoan91@uw.edu>
2016-01-23 14:24:53 -0800

    draft from the assignment 2

---
+++ problem2/CPU.c
@@ -0,0 +1,143 @@
+//
+//  CPU.c
+//  testC
+//
+//  Created by Khoa Doan on 1/23/16.
+//  Copyright © 2016 Khoa Doan. All rights reserved.
+//
+
+#include "CPU.h"
+
+CPU_p CPU_constructor(void) {
+    srand((unsigned) time(NULL));
+    CPU_p myCPU = malloc (sizeof(struct cpu_struct));
+    myCPU->timer = 0;
+    myCPU->pc = 0;
+    myCPU->sysStack = 0;
+    myCPU->cur = NULL;
+    myCPU->createdQueue = FIFO_constructor();
+    myCPU->readyQueue = FIFO_constructor();
+    myCPU->terminateQueue = FIFO_constructor();
+    return myCPU;
+}
+
+void CPU_destructor(CPU_p self) {
+    PCB_destructor(self->cur);
+    FIFO_destructor(self->readyQueue);
+    FIFO_destructor(self->createdQueue);
+    FIFO_destructor(self->terminateQueue);
+    free(self);
+}
+
+void CPU_ISR(CPU_p self, enum interrupt_type itype) {
+    PCB_setState(self->cur, interrupted);
+    PCB_setPC(self->cur, self->pc); 
+    CPU_scheduler(self, itype);
+    CPU_IRET(self);
+}
+
+void CPU_IRET(CPU_p self) {
+    self->pc = self->sysStack;
+}
+
+void CPU_scheduler(CPU_p self, enum interrupt_type itype) {
+    if (itype == timer) {
+        printf("Saving current state of processor to PCB:\n%s", PCB_toString(self->cur));
+        PCB_setState(self->cur, ready);
+        FIFO_enqueue(self->readyQueue, self->cur);
+        CPU_dispatcher(self);
+    } else if (itype == io) {
+        // put to io_queue
+    }
+    // do some additional housekeeping
+}
+
+void CPU_dispatcher(CPU_p self) {
+    //TODO 1.) Save the state of the current process into its PCB (here we mean the PC value)
+    self->cur = FIFO_dequeue(self->readyQueue);
+    PCB_setState(self->cur, running);
+    self->sysStack = PCB_getPC(self->cur);
+    printf("Switching to: %s", PCB_toString(self->cur));
+    printf("Q: %s\n\n\n", FIFO_toString(self->readyQueue));
+}
+
+void mainLoop(CPU_p self) {
+    srand((unsigned) time(NULL));
+    unsigned int random_number_0_to_5;
+    unsigned int pcbID = 1;
+
+    while(pcbID < 30) {
+        // set a timer
+        self->timer = 0;
+
+        // create a new PCB between 0 to 5
+        random_number_0_to_5 = rand() % 6;
+        printf("random number %d\n", random_number_0_to_5);
+        for (int i = 1; i <= random_number_0_to_5; i++) {
+            FIFO_enqueue(self->createdQueue, PCB_constructor(pcbID++, rand() / (RAND_MAX / 16), new, 0, 0));
+        }
+
+        // simulate CPU running
+        //      1. move the pcb from created queue to ready queue.
+        for (int i = 1; i <= random_number_0_to_5; i++) {
+            PCB_p temp = FIFO_dequeue(self->createdQueue);
+            PCB_setState(temp, ready);
+            FIFO_enqueue(self->readyQueue, temp);
+            printf("Process has been enqueued %sQ: %s\n\n", PCB_toString(temp), FIFO_toString(self->readyQueue));
+        }
+        //      2. dequeue the first pcb in the ready queue.
+        if (self->cur == NULL && FIFO_size(self->readyQueue) > 0) {
+            self->cur = FIFO_dequeue(self->readyQueue);
+            PCB_setState(self->cur, running);
+        }
+        //      3. Run the current pcb in the period of the time
+        if (self->pc < 3000) {
+            self->pc = rand() / (RAND_MAX / 1000) + 2000;
+        }
+        while (self->timer < TIME_SLICE) {
+            self->timer++;
+            self->pc++;
+            // if the process need io request, it up-calls here.
+        }
+        // Timer interrupt handler occurs
+        self->sysStack = self->pc; // before it occurs
+        CPU_ISR(self, timer);
+    }
+}
+
+int main() {
+    CPU_p cpu = CPU_constructor();
+    mainLoop(cpu);
+    CPU_destructor(cpu);
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ problem2/PCB.c
@@ -0,0 +1,141 @@
+/*
+ * pcb.c
+ *
+ *  Created on: Jan 7, 2016
+ *      Author: Kyle Doan, nabilfadili
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "pcb.h"
+
+/*
+ * Function:  PCB_constructor
+ * --------------------
+ * Constructs a pcb object, returning a pointer to the pcb object
+ * in the heap
+ *
+ *  params:  pID processID
+ *			 priority
+ *			 s
+ *			 addPC
+ *			 addSp
+ *  returns: PCB_p pointer to the PCB object in the heap
+ */
+PCB_p PCB_constructor(unsigned int pID, unsigned int priority, enum state_type s, unsigned int addPC, unsigned int addSp) {
+    PCB_p result = (PCB_p) malloc(sizeof(PCB));
+    if (result != (PCB_p) NULL) {
+        result->process_num = pID;
+        result->priority = priority;
+        result->state = s;
+        result->addressPC = addPC;
+        result->addressSpace = addSp;
+    }
+    return result;
+}
+
+PCB_p PCB_constructorWithEmpty() {
+	PCB_p result = (PCB_p) malloc(sizeof(PCB));
+	result->priority = -1;
+	return result;
+}
+
+/*
+ * Function:  PCB_destructor
+ * --------------------
+ * Deallocates the memory being used by the passed PCB pointer.
+ *
+ *  params: PCB_p pointer to the PCB object in the heap
+ */
+void PCB_destructor(PCB_p p) {
+	free(p);
+}
+
+/*
+ * Function:  PCB_setProcessNumber
+ * --------------------
+ * Sets the passed PCB object's process_num field to the passed process number.
+ *
+ *  params: PCB_p pointer to the PCB object in the heap and the designated process number.
+ */
+void PCB_setProcessNumber(PCB_p p, unsigned int process_num) {
+	p->process_num = process_num;
+}
+
+int PCB_getProcessNumber(PCB_p p) {
+	return p->process_num;
+}
+
+/*
+ * Function:  PCB_setPriority
+ * --------------------
+ * Sets the passed PCB object's priority field to the passed priority.
+ *
+ *  params: PCB_p pointer to the PCB object in the heap and the designated priority.
+ */
+void PCB_setPriority(PCB_p p, unsigned int priority) {
+	p->priority = priority;
+}
+
+/*
+ * Function:  PCB_getPriority
+ * --------------------
+ * Gets the passed PCB object's priority.
+ *
+ *  returns: The PCB object's priority.
+ */
+int PCB_getPriority(PCB_p p) {
+	return p->priority;
+}
+
+/*
+ * Function:  PCB_setPC
+ * --------------------
+ * Sets the passed PCB object's PC address field to the passed addressPC.
+ *
+ *  params: PCB_p pointer to the PCB object in the heap and the designated priority.
+ */
+void PCB_setPC (PCB_p self, unsigned int pc) {
+    self->addressPC = pc;
+}
+
+/*
+ * Function:  PCB_getPC
+ * --------------------
+ * Gets the passed PCB object's PC address.
+ *
+ *  returns: The PCB object's PC address.
+ */
+int PCB_getPC (PCB_p self) {
+    return self->addressPC;
+}
+
+void PCB_setState (PCB_p self, enum state_type s) {
+    self->state = s;
+}
+
+char* getStateString(enum state_type state) {
+    switch (state) {
+        case new: return "new";
+        case waiting: return "waiting";
+        case ready: return "ready";
+        case running: return "running";
+        case halted: return "halted";
+        case interrupted: return "interrupted";
+    }
+}
+
+/*
+ * Function:  PCB_toString
+ * --------------------
+ * Prints relevant information about the PCB object to the console for
+ * testing/debugging purposes.
+ *
+ * params:	PCB_p pointer to the PCB object in the heap.
+ */
+ char* PCB_toString(PCB_p me) {
+     char * result = (char *) malloc(sizeof(char) * 1000);
+     sprintf(result, "content: PCB_ID: %d, Priority: %d, State: %s, PC: 0x%04X, etc.\n",
+             me->process_num, me->priority, getStateString(me->state), me->addressPC);
+     return result;
+ }
