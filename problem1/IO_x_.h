cd00b337057dd5a9d1908fd6f7d5f6b5bea50e1f
temp <temp@temps-iMac.local>
2016-02-06 11:16:02 -0800

    shells for IO multithread class

---
+++ problem1/IO_x_.h
@@ -0,0 +1,22 @@
+/*
+ * IO_x_.h
+ *
+ *  Created on: Feb 6, 2016
+ *      Author: Kyle
+ */
+
+#ifndef IO_X_H
+#define IO_X_H
+
+#include <time.h>
+#include <unistd.h>
+#include "discontinuities.h"
+
+
+
+
+
+
+
+
+#endif /* IO_X_H */
